391e7099af7d331559e62b1e2afe1920187d8cc7
Sri15521552 <167432193+Sri15521552@users.noreply.github.com>
2024-05-10 21:32:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -127,7 +127,7 @@ DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
 
 
 if SUPPORT_CHANNEL:
-    if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
+    if not re.match("(?:http|https//telegram.me/sparkles_telugu )", SUPPORT_CHANNEL):
         raise SystemExit(
             "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
         )
